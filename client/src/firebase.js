4bdd501f873763a058ffd647c7cca74cf62a6ebf
Richa Sinha <richasinha291@gmail.com>
2024-06-29 15:10:11 +0530

    changes firebae js file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -13,7 +13,6 @@ const firebaseConfig = {
   appId: "1:818961040536:web:51201f9c7c512f30d23ced"
 };
 
-"AIzaSyBmW4Cqd3MKw6v1TDzyeOv7ygKBm8DvmWY"
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
